f7753561351f985241777781a71c94560bc2e53b
Samuel Pastva <sam.pastva@gmail.com>
2025-06-03 20:44:55 +0200

    feat: Add validation functionality for `BmaLayoutContainer`.

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,10 @@ pub use crate::model::bma_network::{BmaNetwork, BmaNetworkError};
 pub use crate::model::bma_relationship::{BmaRelationship, BmaRelationshipError, RelationshipType};
 pub use crate::model::bma_variable::{BmaVariable, BmaVariableError};
 pub use crate::model::layout::bma_layout::BmaLayout;
-pub use crate::model::layout::bma_layout_container::BmaLayoutContainer;
-pub use crate::model::layout::bma_layout_variable::{BmaLayoutVariable, VariableType};
-pub use crate::model::layout::bma_layout_variable::{BmaLayoutVariable, VariableType, BmaLayoutVariableError};
+pub use crate::model::layout::bma_layout_container::{BmaLayoutContainer, BmaLayoutContainerError};
+pub use crate::model::layout::bma_layout_variable::{
+    BmaLayoutVariable, BmaLayoutVariableError, VariableType,
+};
 
 mod validation;
 pub use validation::{
--- src/model/layout/bma_layout_container.rs
+++ src/model/layout/bma_layout_container.rs
@@ -1,10 +1,13 @@
+use crate::utils::{is_blank, is_unique_id};
+use crate::{BmaLayout, ContextualValidation, ErrorReporter};
 use num_rational::Rational64;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 /// Layout information about a container.
 ///
 /// Expected invariants (checked during validation):
-///  - The `id` must be unique within the containers of this [`crate::BmaLayout`].
+///  - The `id` must be unique within the containers of this [`BmaLayout`].
 ///  - If `name` is set, it must not be empty.
 ///
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
@@ -35,5 +38,84 @@ impl BmaLayoutContainer {
             name: Some(name.to_string()),
             ..Default::default()
         }
+    }
+}
+
+#[derive(Error, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum BmaLayoutContainerError {
+    #[error("(Container: `{id}`) Id must be unique within `BmaLayout`")]
+    IdNotUnique { id: u32 },
+    #[error("(Container: `{id}`) Name cannot be empty; use `None` instead")]
+    NameEmpty { id: u32 },
+}
+
+impl ContextualValidation<BmaLayout> for BmaLayoutContainer {
+    type Error = BmaLayoutContainerError;
+
+    fn validate_all<R: ErrorReporter<Self::Error>>(&self, context: &BmaLayout, reporter: &mut R) {
+        // Ensure the name is not empty.
+        if is_blank(&self.name) {
+            reporter.report(BmaLayoutContainerError::NameEmpty { id: self.id });
+        }
+
+        // Ensure that the container id is unique within the enclosing BmaLayout.
+        let Ok(is_unique) = is_unique_id(&context.containers, self, |x| x.id) else {
+            panic!("Validation called on a container that is not part of the BmaLayout")
+        };
+
+        if !is_unique {
+            reporter.report(BmaLayoutContainerError::IdNotUnique { id: self.id });
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{BmaLayout, BmaLayoutContainer, BmaLayoutContainerError, ContextualValidation};
+
+    fn make_layout_for_container(container: &BmaLayoutContainer) -> BmaLayout {
+        BmaLayout {
+            containers: vec![container.clone()],
+            ..Default::default()
+        }
+    }
+
+    #[test]
+    fn default_is_valid() {
+        let container = BmaLayoutContainer::default();
+        let layout = make_layout_for_container(&container);
+        assert!(container.validate(&layout).is_ok());
+    }
+
+    #[test]
+    fn blank_name() {
+        let container = BmaLayoutContainer {
+            name: Some("".to_string()),
+            ..Default::default()
+        };
+        let layout = make_layout_for_container(&container);
+        let issues = container.validate(&layout).unwrap_err();
+        assert_eq!(issues, vec![BmaLayoutContainerError::NameEmpty { id: 0 }]);
+    }
+
+    #[test]
+    fn unique_id() {
+        let container = BmaLayoutContainer::default();
+        let mut layout = make_layout_for_container(&container);
+        layout.containers.push(container.clone());
+        let issues = container.validate(&layout).unwrap_err();
+        assert_eq!(issues, vec![BmaLayoutContainerError::IdNotUnique { id: 0 }]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn missing_container() {
+        let container = BmaLayoutContainer {
+            name: Some("".to_string()),
+            ..Default::default()
+        };
+        let mut layout = make_layout_for_container(&container);
+        layout.containers.clear();
+        container.validate(&layout).unwrap();
     }
 }
